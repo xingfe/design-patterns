209fe955515722bc0ad5ab9dcac5604a78c1f252
Piotr Dereń <piotr.deren@op.pl>
2014-05-14 09:22:50 +0200

    add some explanations

--- singleton/singletonLazyLoading.cpp
+++ singleton/singletonLazyLoading.cpp
@@ -7,6 +7,13 @@
         ~Singleton() {}
   public:
         std::string method() { return "singleton pattern"; }
+	
+	// Be carefull this method is thread safe only since C++11
+	// (full suport). In other cases you should use thread 
+	// synchronization objects but in that case if you 
+        // want you're implementation eficient you should
+	// synchronize only first call of the method.
+
         static Singleton& getInstance()
         {
           static Singleton instance;
