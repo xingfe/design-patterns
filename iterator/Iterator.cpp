5f63b2a13cd96fc30533d3e5fc3c0513bb13f7ca
Piotr Dereń <piotr.deren@op.pl>
2014-06-04 16:52:54 +0200

    iterator design pattern

---
+++ iterator/Iterator.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Iterator
+{
+public:
+	virtual void * first() = 0;
+	virtual void * next() = 0;
+	virtual bool isDone() = 0;
+	virtual void * current() = 0;
+};
+
+class Collection
+{
+public:
+	virtual Iterator& getIterator() = 0;
+};
+
+class ConcreteCollection : public vector<void*>, public Collection
+{
+	Iterator *iterator;
+public:
+	ConcreteCollection() : iterator(NULL) {}
+	~ConcreteCollection() { if (iterator != NULL) delete iterator; }
+	void add(void *e) { push_back(e); }
+	Iterator& getIterator();
+};
+
+class ConcreteIterator : public Iterator
+{
+	ConcreteCollection & collection;
+	size_t position;
+
+public:
+	ConcreteIterator(ConcreteCollection & col) : collection(col) 
+	{
+		position = 0;
+	}
+	void * first()
+	{ 
+		position = 0;
+		return collection[position]; 
+	}
+	void * next() 
+	{ 
+		++position; 
+		if(position < collection.size())
+			return collection[position];
+
+		return NULL;
+
+	}
+	bool isDone() 
+	{
+		if (position < collection.size())
+			return false;
+
+		return true;
+	}
+
+	void * current() { return collection[position]; }
+
+};
+
+Iterator& ConcreteCollection::getIterator()
+{
+	if (iterator == NULL )
+		iterator = new ConcreteIterator(*this);
+
+	return *iterator;
+}
+
+int main()
+{
+	int a = 1, b = 2, c = 3;
+
+	ConcreteCollection colection;
+	colection.add(&a);
+	colection.add(&b);
+	colection.add(&c);
+
+	Iterator &iter = colection.getIterator();
+
+	for (iter.first(); !iter.isDone(); iter.next())
+	{
+		void * pItem = iter.current();
+		cout << *(static_cast<int*>(pItem)) << endl;
+	}
+
+	return 0;
+}
